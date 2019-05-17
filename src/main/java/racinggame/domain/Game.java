dd5cdd42956dec1bb33c44f7980036362bbde35d
Soojin Roh <sooreal91@gmail.com>
2019-05-17 16:47:40 +0900

    refactor Game 클래스의 모델 로직과 출력 기능 분리
    - AppController를 만들어 Game 클래스에서 출력하던 기능을 분리

--- src/main/java/racinggame/domain/Game.java
+++ src/main/java/racinggame/domain/Game.java
@@ -9,14 +9,14 @@ public class Game {
 
     private static final int MAX_RANDOM_VALUE = 10;
 
-    public List<Car> playGames(List<Car> cars, int gameCount) {
-        for (int i = 0; i < gameCount; i++) {
-            cars = playGame(cars);
-            ResultOutput.eachGameOutput(cars);
-        }
-
-        return cars;
-    }
+//    public List<Car> playGames(List<Car> cars, int gameCount) {
+//        for (int i = 0; i < gameCount; i++) {
+//            cars = playGame(cars);
+//            ResultOutput.eachGameOutput(cars);
+//        }
+//
+//        return cars;
+//    }
 
     public List<Car> playGame(List<Car> cars ) {
         for (Car car : cars) {
